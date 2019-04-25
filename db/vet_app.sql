7675c227df103ce4c5f4b81caed95245bf909d10
Rhys Bowen Harries <rbharries@gmail.com>
2019-04-25 15:12:59 +0100

    tables created

---
+++ db/vet_app.sql
@@ -0,0 +1,16 @@
+DROP TABLE animals;
+DROP TABLE vets;
+
+CREATE TABLE vets (
+  id SERIAL8 primary key,
+  name VARCHAR(255)
+);
+
+CREATE TABLE animals (
+  id SERIAL8 primary key,
+  name VARCHAR(255),
+  Stardate_of_birth VARCHAR(255),
+  Type_of_Animal VARCHAR(255),
+  Owner_information VARCHAR(255),
+  Treatment_Notes VARCHAR(255)
+);
